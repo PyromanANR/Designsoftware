bea864a55d3a857d82452b0b10e9b216cde2bb3d
alevchenko12 <nastiya.levchenko@gmail.com>
2025-04-01 12:06:33 +0200

    Loop test diagram and expected code.

--- translation/codePython/generated_code.py
+++ translation/codePython/generated_code.py
@@ -5,7 +5,7 @@ import threading
 # Спільні змінні
 x = 12
 y = 0
-z = 120
+z = 0
 A = 5
 B = 2
 
@@ -29,11 +29,22 @@ def thread_Diagram_2():
             print('B =', B)
         # Кінець виконання
 
+def thread_Diagram_3():
+    with lock:
+        # Початок виконання
+        while True:
+            y = 1
+            print('y =', y)
+            print('x =', x)
+            x = int(input())
+
 if __name__ == "__main__":
     t_Diagram_1 = threading.Thread(target=thread_Diagram_1)
     t_Diagram_1.start()
     t_Diagram_2 = threading.Thread(target=thread_Diagram_2)
     t_Diagram_2.start()
+    t_Diagram_3 = threading.Thread(target=thread_Diagram_3)
+    t_Diagram_3.start()
 
     for t in threading.enumerate():
         if t is not threading.current_thread():
